873f7e45ebe7f824f03a9eb4ba780ae506a37da8
Roberto Gentili <roberto.gentili@nttdata.com>
2022-08-04 10:04:37 +0200

    Improved functionalities

--- app/src/main/java/org/rg/util/RestTemplateSupplier.java
+++ app/src/main/java/org/rg/util/RestTemplateSupplier.java
@@ -1,9 +1,15 @@
 package org.rg.util;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Consumer;
 
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -16,6 +22,7 @@ public class RestTemplateSupplier {
     private static RestTemplateSupplier sharedInstance;
     private RestTemplate restTemplate;
     private Consumer<HttpClientBuilder> httpClientBuilderSetter;
+    private boolean requestLoggerEnabled;
 
     private RestTemplateSupplier(Consumer<HttpClientBuilder> httpClientBuilderSetter) {
         this();
@@ -76,14 +83,66 @@ public class RestTemplateSupplier {
                             } catch (HttpClientErrorException | HttpServerErrorException exc) {
                                 LoggerChain.getInstance().logError("Http response error: " + exc.getStatusCode().value() + " (" + exc.getStatusText() + "). Body: " + exc.getResponseBodyAsString());
                                 throw exc;
+                            } catch (Throwable exc) {
+                                LoggerChain.getInstance().logError("Exception occurred: " + exc.getMessage());
+                                throw exc;
                             }
                         }
                     });
                     this.restTemplate = restTemplate;
+                    activateOrDeactivateRequestLogger();
                 }
             }
         }
         return restTemplate;
     }
 
+    public synchronized RestTemplateSupplier enableRequestLogger() {
+        requestLoggerEnabled = true;
+        activateOrDeactivateRequestLogger();
+        return this;
+    }
+
+    public synchronized RestTemplateSupplier disableRequestLogger() {
+        requestLoggerEnabled = false;
+        activateOrDeactivateRequestLogger();
+        return this;
+    }
+
+    private synchronized void activateOrDeactivateRequestLogger() {
+        if (restTemplate == null) {
+            return;
+        }
+        List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+        if (requestLoggerEnabled) {
+            if (interceptors == null) {
+                interceptors = new CopyOnWriteArrayList<>();
+                restTemplate.setInterceptors(interceptors);
+            }
+            if (!interceptors.stream().filter(interceptor -> interceptor instanceof RequestLogger).findFirst().isPresent()) {
+                interceptors.add(new RequestLogger());
+            }
+        } else {
+            if (interceptors == null) {
+                return;
+            }
+            ClientHttpRequestInterceptor requestLogger = interceptors.stream().filter(interceptor -> interceptor instanceof RequestLogger).findFirst().orElseGet(() -> null);
+            if (requestLogger != null) {
+                interceptors.remove(requestLogger);
+            }
+        }
+    }
+
+    private static class RequestLogger implements ClientHttpRequestInterceptor {
+        @Override
+        public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+            String logString =
+                "Method: " + request.getMethod().toString() + " - " +
+                "URI: " + request.getURI() +
+                (!request.getHeaders().isEmpty()? " - Headers: " + request.getHeaders().toString() : "") +
+                (body.length > 0? " - body: " + new String(body, StandardCharsets.UTF_8) : "");
+            LoggerChain.getInstance().logDebug(logString);
+            return execution.execute(request, body);
+        }
+    }
 }
