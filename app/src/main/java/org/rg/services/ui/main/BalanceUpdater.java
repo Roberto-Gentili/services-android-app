e3f02d65e809a2a161a3c994a909c816c2638c80
Roberto Gentili <roberto.gentili@nttdata.com>
2022-09-03 09:56:21 +0200

    Update

--- app/src/main/java/org/rg/services/ui/main/BalanceUpdater.java
+++ app/src/main/java/org/rg/services/ui/main/BalanceUpdater.java
@@ -34,7 +34,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
 class BalanceUpdater {
-    private static Long seedForCharts;
+    private /*static*/ Long seedForCharts;
     private final MainFragment fragment;
     private AsyncLooper updateTask;
     private PieChartManager balancesChartManager;
